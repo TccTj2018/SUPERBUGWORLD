c57b5080db195eaecfd936b15ba8215cd4f2f398
MARCIO MAURICIO DOS SANTOS <marcio.msantos8@senacsp.edu.br>
2018-11-01 21:49:38 -0300

    Inclui scripts troca de cenas...

---
+++ Assets/Scripts/Scenary/Fase1/TrocaCena.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocaCena : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Player")
+        {
+
+        }
+
+        SceneManager.LoadScene(1);
+    }
+}
