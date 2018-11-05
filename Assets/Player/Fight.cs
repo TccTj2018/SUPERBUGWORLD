a5c46ff5bd63d99eb4538dea63786e7aa245ca60
MAURILIO SALES SILVA REIS <maurilio.ssreis@senacsp.edu.br>
2018-11-05 20:56:38 -0200

    personagem principal atualizado

---
+++ Assets/Player/Fight.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fight : MonoBehaviour {
+
+    static Animator anim;
+    bool idle;
+    bool soco1 = false;
+    bool soco2 = false;
+    bool soco3 = false;
+     public AudioSource socoAudio;
+    public GameObject prefab;
+    public GameObject prefab2;
+    public Transform spawpoint;
+    public Transform pai;
+    void Start () {
+        anim = GetComponent<Animator>();
+        idle = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && soco1 == false ) {
+            socoAudio.Play();
+            anim.SetTrigger("soco1");
+            soco2 = true;
+            GameObject ObjetoInstanciado = Instantiate(prefab2, spawpoint.position, spawpoint.rotation, pai) as GameObject;
+            if (Input.GetButtonDown("Fire1") && soco1 == false && soco2 == true)
+            {
+
+                anim.SetTrigger("soco2");
+                soco3 = true;
+                if (Input.GetButtonDown("Fire1") && soco1 == false && soco2 == true && soco3 == true)
+                {
+
+                    anim.SetTrigger("soco3");
+                    soco2 = true;
+                }
+            }
+        }
+        if (Input.GetButtonDown("Fire2") )
+        {
+
+            anim.SetTrigger("soco4");
+            GameObject ObjetoInstanciado = Instantiate(prefab, spawpoint.position, spawpoint.rotation,pai) as GameObject;
+           
+        }
+    }
+
+    
+}
---
+++ Assets/Player/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    public Transform player;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.rotation = player.rotation;
+	}
+}
